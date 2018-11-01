f78b0d134dc3b42fb74f6d43289a39682f62cfa0
Luke Shiner <luke@lukeshiner.com>
2018-11-01 12:04:07 +0000

    fix: Fix ImportError

--- src/ccp_api/__init__.py
+++ src/ccp_api/__init__.py
@@ -1,6 +1,6 @@
 """ccp_api - Cloud Commerce Pro API integration for Python."""
 
-from .credentials import login  # NOQA
+from .credentials import Credentials  # NOQA
 from .endpoint import Endpoint  # NOQA
 
 from .endpoints import products  # NOQA
@@ -8,3 +8,5 @@ from .endpoints import products  # NOQA
 from .__version__ import __title__, __description__, __url__  # NOQA
 from .__version__ import __version__, __author__, __author_email__  # NOQA
 from .__version__ import __copyright__, __license__, __release__  # NOQA
+
+login = Credentials()
